5dc710727e1fa0e38d2d3e412efab6310856add4
zliu <zliu92@hawk.iit.edu>
2016-11-05 13:55:09 -0500

    update file by lzp

--- file.py
+++ file.py
@@ -14,10 +14,8 @@ for i in range(0,len(content)-1):
 
 from sklearn.preprocessing import scale
 data = scale(data,copy = 'False')
-print(data[0])
-"""
+
 from sklearn.linear_model import LogisticRegression
-clf = LogisticRegression(penalty = 'l1')
+clf = LogisticRegression(penalty = 'l1',random_state = 0)
 clf = clf.fit(data,label)
 print (clf.coef_[0])
-"""

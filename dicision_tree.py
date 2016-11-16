a8417f8d727c2b3e51d02c739888e4719a4472dc
zliu <zliu92@hawk.iit.edu>
2016-11-15 20:56:18 -0600

    evaluation

--- dicision_tree.py
+++ dicision_tree.py
@@ -15,7 +15,7 @@ for i in range(0,len(content)-1):
 from sklearn.preprocessing import scale
 data = scale(data,copy = 'False')
 
-from sklearn.linear_model import LogisticRegression
-clf = LogisticRegression(penalty = 'l1',random_state = 0)
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
 clf = clf.fit(data,label)
-print (clf.coef_[0])
+tree.export_graphviz(clf,out_file='tree.dot') 
